a82cb107709fdaf99d15556cc276fa47501307c4
Deon Odendaal <deon.odendaal@protonmail.com>
2019-11-21 17:44:06 +0000

    welcome screen

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import "./Welcome.css";
 
 const Welcome = props => {
   return (
     <div>
-      <div id="background">
-        <button
-          onClick={() => props.setCurrentDisplay("register")}
-          className="btn btn-warning"
-        >
-          Register
-        </button>
+      <div
+        id="background"
+        className="d-flex flex-column justify-content-around"
+      >
+        <h1 id="welcome-heading" className="d-1 text-warning text-center">
+          Star Love
+        </h1>
+        <div id="buttonContainer">
+          <button
+            onClick={() => props.setCurrentDisplay("register")}
+            className="btn btn-warning text-light"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
