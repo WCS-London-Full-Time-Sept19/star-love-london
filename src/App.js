6cc249719b7b5a295314315c8aa5b20e267c2647
anap <anapcwb@gmail.com>
2019-11-21 14:48:26 +0000

    add viewUserProfile component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import UserProfile from "./viewUserProfile";
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
 }
 
 export default App;
